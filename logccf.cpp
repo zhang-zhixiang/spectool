18b91040d6b27110e5b7b5e77e23a403cfe8513d
zzx <zzx@bigtree.xyz>
2020-09-11 19:03:09 +0800

    cleanup code

--- logccf.cpp
+++ logccf.cpp
@@ -20,19 +20,17 @@ private:
     Shift_spec(const Shift_spec&);
     Shift_spec& operator=(const Shift_spec&); //prevent copy
     std::complex<double> * _in, * _out;
-    std::complex<double> * _fourtr;
+    std::complex<double> * _fourtr, *_four_with_shift;
     fftw_plan _p;
     fftw_plan _ifft;
     size_t _size;
     double * _sig;
-    std::complex<double> * _complexarr;
     double _shift;
     void ini_par(int size);
     void destroy_par();
 public:
     double * specout;
 
-    // Shift_spec();
     Shift_spec(CVEC & spec);
     bool set_spec(CVEC & spec);
     double get_shift_value();
@@ -41,33 +39,30 @@ public:
     ~Shift_spec();
 };
 
-// Shift_spec::Shift_spec():
-//     _in(nullptr), _out(nullptr), _p(nullptr),
-//     _size(0), _sig(nullptr),
-//     _complexarr(nullptr), _shift(0){}
-
 Shift_spec::Shift_spec(CVEC & spec):
-    _in(nullptr), _out(nullptr), _p(nullptr),
-    _size(0), _sig(nullptr),
-    _complexarr(nullptr), _shift(0){
-        // int a = 23;
-    // Shift_spec();
+    _in(nullptr), _out(nullptr), _fourtr(nullptr),
+    _four_with_shift(nullptr), 
+    _p(nullptr), _ifft(nullptr),
+    _size(0), _sig(nullptr), _shift(0){
     set_spec(spec);
 }
 
 void Shift_spec::destroy_par(){
     if (_in != nullptr){
         fftw_destroy_plan(_p);
-        // fftw_free(_in);
-        // fftw_free(_out);
+        fftw_destroy_plan(_ifft);
         delete [] _in;
         delete [] _out;
+        delete [] _fourtr;
+        delete [] _four_with_shift;
         delete [] _sig;
-        delete [] _complexarr;
         delete [] specout;
         _p = nullptr;
+        _ifft = nullptr;
         _in = nullptr;
         _out = nullptr;
+        _fourtr = nullptr;
+        _four_with_shift = nullptr;
         _sig = nullptr;
         specout = nullptr;
         _size = 0;
@@ -86,16 +81,16 @@ void Shift_spec::ini_par(int size){
     _in = new std::complex<double>[_size];
     _out = new std::complex<double>[_size];
     _fourtr = new std::complex<double>[_size];
+    _four_with_shift = new std::complex<double>[_size];
     _ifft = fftw_plan_dft_1d(_size,
                              reinterpret_cast<fftw_complex*>(_in),
                              reinterpret_cast<fftw_complex*>(_fourtr),
                              FFTW_BACKWARD, FFTW_ESTIMATE);
     _p = fftw_plan_dft_1d(_size,
-                          reinterpret_cast<fftw_complex*>(_fourtr),
+                          reinterpret_cast<fftw_complex*>(_four_with_shift),
                           reinterpret_cast<fftw_complex*>(_out),
                           FFTW_FORWARD, FFTW_ESTIMATE);
     _sig = new double[_size];
-    _complexarr = new std::complex<double>[_size];
     specout = new double[_size];
 
     double doublesize = double(_size);
@@ -113,39 +108,18 @@ bool Shift_spec::set_spec(CVEC & spec){
         reinterpret_cast<double*>(_in)[2*ind] = spec[ind];
         reinterpret_cast<double*>(_in)[2*ind+1] = 0;
     }
-    std::cout << "_in =" << std::endl;
-    for(size_t ind = 0; ind < _size; ++ind)
-        std::cout << _in[ind] << "  ";
-    std::cout << std::endl;
+    fftw_execute(_ifft);
     return true;
 }
 
 double * Shift_spec::get_shift_spec(double shift){
     _shift = shift;
-    std::cout << "_size = " << _size << std::endl;
-    // for (auto var : _in)
-    //     std::cout << var << "  ";
-    // std::cout << std::endl;
-    fftw_execute(_ifft);
-    std::cout << "_fourtr =" << std::endl;
-    for(size_t ind = 0; ind < _size; ++ind)
-        std::cout << _fourtr[ind] << "  ";
-    std::cout << "after run fftw_execute" << std::endl;
-    std::cout << "sincos =" << std::endl;
     for (size_t ind = 0; ind < _size; ++ind){
         auto a = cos(_sig[ind]*shift);
         auto b = sin(_sig[ind]*shift);
         std::complex<double> complexshift = {a, b};
-        std::cout << complexshift << "  ";
-        _fourtr[ind] = _fourtr[ind] * complexshift;
-        // reinterpret_cast<double*>(_complexarr)[2*ind] = a;
-        // reinterpret_cast<double*>(_complexarr)[2*ind + 1] = b;
+        _four_with_shift[ind] = _fourtr[ind] * complexshift;
     }
-    std::cout << std::endl;
-    std::cout << "_fourtr =" << std::endl;
-    for(size_t ind = 0; ind < _size; ++ind)
-        std::cout << _fourtr[ind] << "  ";
-    std::cout << std::endl;
     fftw_execute(_p);
     for(size_t ind = 0; ind < _size; ++ind)
         specout[ind] = std::abs(_out[ind]) / _size;
@@ -153,7 +127,6 @@ double * Shift_spec::get_shift_spec(double shift){
 }
 
 VEC Shift_spec::get_shift_spec_arr(double shift){
-    std::cout << "run get_shift_spec_arr" << std::endl;
     get_shift_spec(shift);
     VEC out(_size);
     for(size_t ind = 0; ind < _size; ++ind)
@@ -162,7 +135,7 @@ VEC Shift_spec::get_shift_spec_arr(double shift){
 }
 
 template < typename Iter, typename Iterb>
-auto get_ccf(Iter begin, Iter end, Iterb begin_ref, bool mult=true){
+auto get_ccf_value(Iter begin, Iter end, Iterb begin_ref, bool mult=true){
     typename std::iterator_traits<Iter>::value_type out = 0;
     if(mult == true){
         for(size_t ind = 0; begin+ind != end; ++ind)
@@ -175,15 +148,6 @@ auto get_ccf(Iter begin, Iter end, Iterb begin_ref, bool mult=true){
     }
     return out;
 }
-
-// int main(){
-//     VEC a{1,2,3,4};
-//     VEC b{1,2,3,4};
-//     // auto data = std::accumulate(a.begin(), a.end())
-//     auto out = get_ccf(a.begin(), a.end(), b.begin());
-//     return 0;
-// }
-
 
 auto get_shift(CVEC & spec, CVEC & spec_ref, double left_edge, double right_edge, double resolution, bool mult=true){
     // return shift_peak, rmax
@@ -198,7 +162,7 @@ auto get_shift(CVEC & spec, CVEC & spec_ref, double left_edge, double right_edge
         auto sfrom = spec.begin() + range;
         auto send = spec.end() - range;
         auto tfrom = spec_ref.begin() + range + shift;
-        auto r = get_ccf(sfrom, send, tfrom, mult);
+        auto r = get_ccf_value(sfrom, send, tfrom, mult);
         rlst.push_back(r);
     }
     int indminmax = 0;
@@ -213,12 +177,14 @@ auto get_shift(CVEC & spec, CVEC & spec_ref, double left_edge, double right_edge
     if (resolution > 1)
         return std::make_tuple(double(aprox_shift), rlst[indminmax]);
     VEC rlst2, shiftlst2;
+    Shift_spec shiftmodel(spec_ref);
     for(double shift = aprox_shift-1; shift < aprox_shift+1; shift+=resolution){
         shiftlst2.push_back(shift);
+        shiftmodel.get_shift_spec(shift);
         auto sfrom = spec.begin() + range;
         auto send = spec.end() - range;
-        auto tbegin = spec_ref.begin();
-        auto r = get_ccf(sfrom, send, tbegin, mult);
+        auto tbegin = shiftmodel.specout + range;
+        auto r = get_ccf_value(sfrom, send, tbegin, mult);
         rlst2.push_back(r);
     }
     if (mult == true){
@@ -241,5 +207,5 @@ PYBIND11_MODULE(liblogccf, m) {
         .def(py::init<CVEC>())
         .def("get_shift_spec_arr", &Shift_spec::get_shift_spec_arr);
 
-    // m.def("get_shift", &get_shift, "run FR/RSS to estimate the error bar");
+    m.def("get_shift", &get_shift, "run FR/RSS to estimate the error bar");
 }
