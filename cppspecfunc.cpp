41c2c12020fd9af3811cadf77893140e0099709d
zzx <zzx@bigtree.xyz>
2020-09-17 10:03:22 +0800

    continuum code

--- cppspecfunc.cpp
+++ cppspecfunc.cpp
@@ -1,6 +1,15 @@
 #include "cppspecfunc.h"
 
 
+VEC normalize_wave(CVEC & wave){
+    double med = (wave.front() + wave.back()) / 2;
+    double length = wave.back() - wave.front();
+    VEC out(wave.size());
+    for(size_t ind = 0; ind < wave.size(); ++ind)
+        out[ind] = (wave[ind] - med) / length * 1.9999999;
+    return out;
+}
+
 Continuum::Continuum(CVEC & wave, CVEC & flux){
-    _wave = wave
+    _wave = wave;
 }
--- cppspecfunc.h
+++ cppspecfunc.h
@@ -3,12 +3,19 @@
 
 #include <memory>
 #include "types.h"
+#include "gsl/gsl_multifit.h"
 
 VEC normalize_wave(CVEC & wave);
 
 class Continuum{
+    gsl_multifit_robust_workspace * work;
+    gsl_matrix * X, * cov;
+    gsl_vector * x, * y, * c;
     VEC _wave;
     VEC _flux;
+    int _order;
+    void ini_gsl(int specsize, int order);
+    void remove_gsl();
 
 public:
     Continuum(CVEC & wave, CVEC & flux);
@@ -21,7 +28,7 @@ public:
     VEC get_norm_spec();
     py::array_t<double> get_continuum_arr();
     py::array_t<double> get_norm_spec_arr();
-}
+};
 
 
 #endif // !__CPPSPECFUNC_H__
