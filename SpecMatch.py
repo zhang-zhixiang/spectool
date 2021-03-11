ebdbfd3f983325c214dd0b95642ec59c385c0c4a
zzx <zzx@bigtree.xyz>
2021-03-11 16:05:18 +0800

    add a SpecTransform class

--- SpecMatch.py
+++ SpecMatch.py
@@ -3,7 +3,6 @@ from . import spec_func
 from . import spec_filter
 from . import convol
 from lmfit import Parameters
-from lmfit.models import GaussianModel
 
 
 def get_scale_model(degree=15):
@@ -38,68 +37,120 @@ def get_fwhm_model(degree=2):
     return get_fwhm, lmpars
 
 
-def get_shift_model(degree=1, type='velocity'):
+def get_shift_model(degree=1):
     parshift = ['wshift%d' % ind for ind in range(degree)]
 
-    def get_shifted_wave(wave, pars):
+    def get_shift(wave, pars):
         arrpar = np.array([pars[key].value for key in parshift])
-        if arrpar.size == 1 and type == 'velocity':
-            return spec_func.shift_wave(wave, arrpar[0])
-        else:
-            return spec_func.shift_wave_mutable(wave, arrpar, type)
+        nwave = wave / 10000.0
+        shift = np.zeros(nwave.shape)
+        for ind, pari in enumerate(arrpar):
+            shift = shift + pari * nwave**ind
+        return shift
 
     lmpars = Parameters()
     for parname in parshift:
         lmpars.add(parname, 0.0)
 
-    return get_shifted_wave, lmpars
+    return get_shift, lmpars
 
 
-class SpecMatch:
-    def __init__(self, wave, flux, wave_temp, flux_temp, degree_scale=15,
-                 degree_fwhm=1, degree_shift=1, shifttype='velocity'):
-        self.set_spectrum(wave, flux)
-        self.set_template(wave_temp, flux_temp)
-        self.func_scale, parscale = get_scale_model(degree_scale)
-        self.func_fwhm, parfwhm = get_fwhm_model(degree_fwhm)
-        self.func_shift, parshift = get_shift_model(degree_shift, shifttype)
-        self._parameters = parscale + parfwhm + parshift
+class SpecTransform:
+    def __init__(self, wave, flux, degree_scale=15, degree_fwhm=1,
+                 degree_shift=1, shift_type='velocity'):
+        """transform a spectrum to a new style with given parameters.
+        The transform include wavelength shift, velocity broading, flux
+        scale. The class is designed to fit and match two different spectra.
 
-    def get_parameters(self):
-        return self._parameters
+        the broading unit is km/s
+        the shift can work in two unit -- velocity: km/s, lambda: lambda(AA)
+        Here we assume the wavelength is in the unit of AA
 
-    def res(self, pars, mask=None):
-        _res = self._flux - self.get_flux(pars)
-        if mask is not None:
-            arg = spec_func.mask_wave(self._wave, mask)
-            _res = _res[arg]
-        return _res
+        An advice: if the flux is normalized before using, like flux = flux / np.median(flux),
+        the scale par will be fited more easily. Or you can choose fit in log scale.
 
-    def get_flux(self, pars):
-        scale = self.get_scale(pars)
-        fwhmlst = self.get_fwhm(pars)
-        nrefwave = self.get_ref_newave(pars)
-        fluxref_rebin = np.array(spec_func.rebin.rebin(nrefwave, self._fluxref, self._wave))
-        fluxref_fwhm = spec_filter.gauss_filter_mutable(self._wave, fluxref_rebin, fwhmlst)
-        return fluxref_fwhm * scale
 
-    def get_scale(self, pars):
-        return self.func_scale(self._wave, pars) / self._unitr * self._unitf
-
-    def get_fwhm(self, pars):
-        return self.func_fwhm(self._wave, pars)
-
-    def get_ref_newave(self, pars):
-        return self.func_shift(self._waveref, pars)
-
-    def set_spectrum(self, wave, flux, error=None):
+        Args:
+            wave (numpy.ndarray(float64)): spec wave (will be transformed)
+            flux (numpy.ndarray(float64)): spec flux (will be transformed)
+            degree_scale (int or None, optional): the degree of scale poly, if set to None, the scale function will be closed. Defaults to 15.
+            degree_fwhm (int or None, optional): the degree of broading poly, if set to None, the broading function will be closed. Defaults to 1.
+            degree_shift (int or None, optional): the degree of wavelength shift, if set to None, the shift function will be closed. Defaults to 1.
+            shift_type (str, optional): the shift mode of the wavelength, 'velocity' or 'lambda'. Defaults to 'velocity'.
+        """
         self._wave = wave.copy()
         self._flux = flux.copy()
-        self._unitf = spec_func.get_unit(self._flux)
-        self._fluxunit = self._flux / self._unitf
+        parall = None
+        if degree_scale is not None:
+            self.func_scale, parscale = get_scale_model(degree_scale)
+        else:
+            self.func_scale = None
+            parscale = Parameters()
 
-    def set_template(self, wave, flux):
-        self._waveref = wave
-        self._fluxref = flux
-        self._unitr = spec_func.get_unit(self._fluxref)
-        self._fluxrefunit = self._fluxref / self._unitr
+        if degree_fwhm is not None:
+            self.func_fwhm, parfwhm = get_fwhm_model(degree_fwhm)
+        else:
+            self.func_fwhm = None
+            parfwhm = Parameters()
+        
+        if degree_shift is not None:
+            self.func_shift, parshift = get_shift_model(degree_shift)
+        else:
+            self.func_shift = None
+            parshift = Parameters()
+        parall = parscale + parfwhm + parshift
+        self._parameters = parall
+        self._shift_type  = shift_type
+
+    def get_parameters(self):
+        """return the lmfit.Parameters object, which can be used to transform this object.
+
+        Returns:
+            lmfit.Parameters: the pars is a pair of this object
+        """
+        return self._parameters
+
+    def get_flux(self, pars, wave):
+        """for given pars and wave, the function will return flux array after a transform
+
+        Args:
+            pars (lmfit.Parameters): pars used to transform this object, paramery pars should get from get_parameters function
+            wave (numpy.ndarray(float64)): wavelength array
+
+        Returns:
+            numpy.ndarray: the transformed flux
+        """
+        if self.func_shift is not None:
+            shiftlst = self.func_shift(wave, pars)
+            nwave_temp = spec_func.shift_wave_mutable(self._wave, shiftlst, shifttype=self._shift_type)
+        else:
+            nwave_temp = self._wave
+        nflux_temp = spec_func.rebin.rebin_padvalue(nwave_temp, self._flux, wave)
+
+        if self.func_fwhm is not None:
+            arrvelocity = self.func_fwhm(wave, pars)
+            nflux_temp = spec_filter.gauss_filter_mutable(wave, nflux_temp, arrvelocity)
+
+        if self.func_scale is not None:
+            scale = self.func_scale(wave, pars)
+            nflux_temp = nflux_temp * scale
+        
+        return nflux_temp
+
+    def get_fwhm(self, pars, wave):
+        if self.func_fwhm is not None:
+            return self.func_fwhm(wave, pars)
+        else:
+            return None
+
+    def get_scale(self, pars, wave):
+        if self.func_scale is not None:
+            return self.func_scale(wave, pars)
+        else:
+            return None
+
+    def get_shift(self, pars, wave):
+        if self.func_shift is not None:
+            return self.func_shift(wave, pars)
+        else:
+            return None
