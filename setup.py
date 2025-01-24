778bf6e65f2a0e83c38a23ca233b99e9b0c87042
zzx <zzx@bigtree.xyz>
2025-01-24 16:15:21 +0800

    check whether gsl>2.17 fftw3 pybind11 are installed

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import glob
 from setuptools import setup
+import subprocess
 
 curdir = os.getcwd()
 sdir = os.path.dirname(os.path.abspath(__file__))
@@ -11,6 +12,42 @@ os.chdir(codedir)
 #     # remove old .so files before building, avoid to use the old .so files
 #     os.remove(f)
 
+
+def check_gsl():
+    try:
+        output = subprocess.check_output(['pkg-config', '--modversion', 'gsl'], text=True)
+        installed_version = output.strip()
+        required_version = '2.7'
+        if installed_version < required_version:
+            raise Exception(f'gsl version < {required_version}')
+    except subprocess.CalledProcessError:
+        raise ValueError('gsl is not installed')
+    print('gsl version =', installed_version)
+
+
+def check_fftw3():
+    try:
+        output = subprocess.check_output(['pkg-config', '--modversion', 'fftw3'], text=True)
+        installed_version = output.strip()
+        # required_version = '3.3.10'
+        # if installed_version < required_version:
+        #     raise Exception(f'fftw3 version < {required_version}')
+    except subprocess.CalledProcessError:
+        raise ValueError('fftw3 is not installed')
+    print('fftw3 version =', installed_version)
+
+
+def check_pybind11():
+    try:
+        import pybind11
+    except ImportError:
+        raise ValueError('pybind11 is not installed')
+    print('pybind11 is installed')
+
+
+check_gsl()
+check_fftw3()
+check_pybind11()
 python_version = 'python' + str(os.sys.version_info[0]) + '.' + str(os.sys.version_info[1])
 os.environ['PYTHON_VERSION'] = python_version
 print(f"python version is set to: {os.environ['PYTHON_VERSION']}")
